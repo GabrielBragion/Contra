ab94f29229bf07811ad40f99f40ad9fe2bb847a6
Gabriel Bragion <gabriel.bragion.micco@gmail.com>
2025-03-10 15:03:28 +0000

    setting db

--- src/contra/settings.py
+++ src/contra/settings.py
@@ -122,10 +122,19 @@ WSGI_APPLICATION = "contra.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+ """
+ 
+#LOCAL DB PRODUCAO
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "/opt/render/db.sqlite3",  # Local persistente
     }
 }
 
