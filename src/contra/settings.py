e3864c57821c3fe880a13e284c10eefe69f153af
Gabriel Bragion <gabriel.bragion.micco@gmail.com>
2025-03-10 14:47:00 +0000

    corrigido statis

--- src/contra/settings.py
+++ src/contra/settings.py
@@ -164,8 +164,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Onde os arquivos coletados serão armazenados
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Onde os arquivos estáticos ficam antes da coleta
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
